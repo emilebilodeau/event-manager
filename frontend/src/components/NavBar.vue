7ea17c8ede90c90e7a43647fa5cd19c56c7591d2
emilebilodeau <emilebilodeau@gmail.com>
2025-02-18 11:51:37 -0800

    simplified navbar html

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -10,44 +10,36 @@ const isActiveLink = (routePath: string) => {
 <template>
   <header>
     <nav class="bg-blue-700 border-b border-blue-500">
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div class="flex h-20 items-center justify-between">
-          <div
-            class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+      <div
+        class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 flex h-20 items-center justify-between"
+      >
+        <div class="flex space-x-2 ml-auto">
+          <RouterLink
+            to="/"
+            :class="[
+              isActiveLink('/') ? 'bg-blue-900' : '',
+              'hover:bg-gray-900',
+              'text-white',
+              'px-3',
+              'py-2',
+              'rounded-md',
+            ]"
+            >Home</RouterLink
           >
-            <div class="md:ml-auto">
-              <div class="flex space-x-2">
-                <RouterLink
-                  to="/"
-                  :class="[
-                    isActiveLink('/') ? 'bg-blue-900' : '',
-                    'hover:bg-gray-900',
-                    'text-white',
-                    'px-3',
-                    'py-2',
-                    'rounded-md',
-                  ]"
-                  >Home</RouterLink
-                >
-                <RouterLink
-                  to="/events"
-                  :class="[
-                    isActiveLink('/events') ? 'bg-blue-900' : '',
-                    'hover:bg-gray-900',
-                    'text-white',
-                    'px-3',
-                    'py-2',
-                    'rounded-md',
-                  ]"
-                  >Events</RouterLink
-                >
-              </div>
-            </div>
-          </div>
+          <RouterLink
+            to="/events"
+            :class="[
+              isActiveLink('/events') ? 'bg-blue-900' : '',
+              'hover:bg-gray-900',
+              'text-white',
+              'px-3',
+              'py-2',
+              'rounded-md',
+            ]"
+            >Events</RouterLink
+          >
         </div>
       </div>
     </nav>
   </header>
 </template>
-
-<style></style>
